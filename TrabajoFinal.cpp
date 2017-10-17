f5da384b39b1cb1e156cb4dd94dbed5e121aa3a4
abadia962 <abadia962@gmail.com>
2017-10-16 23:12:58 -0500

    Agregando

--- TrabajoFinal.cpp
+++ TrabajoFinal.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+/*
+Variables
+Son 20 Empleados
+Leer a Cada uno
+Nombre, Horas de Trabajo, Base de Pago, Edad
+
+Otras Variables
+Impuestos, Saldo Neto
+*/
 
 struct Trabajadores{
 	string nombre;
